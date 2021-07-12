e59ef60d628cc4ff0595a400efaa071b839bda64
Jorge Gutiérrez <34510304+Yorchly@users.noreply.github.com>
2021-07-12 21:34:01 +0200

    Changes in buttons

--- ChillExe/Controls/ElementsList.Designer.cs
+++ ChillExe/Controls/ElementsList.Designer.cs
@@ -44,9 +44,10 @@ namespace ChillExe.Controls
             // 
             // saveButton
             // 
-            this.saveButton.Location = new System.Drawing.Point(495, 69);
+            this.saveButton.Location = new System.Drawing.Point(566, 92);
+            this.saveButton.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.saveButton.Name = "saveButton";
-            this.saveButton.Size = new System.Drawing.Size(75, 23);
+            this.saveButton.Size = new System.Drawing.Size(86, 31);
             this.saveButton.TabIndex = 1;
             this.saveButton.Text = "Save";
             this.saveButton.UseVisualStyleBackColor = true;
@@ -54,9 +55,10 @@ namespace ChillExe.Controls
             // 
             // exportButton
             // 
-            this.exportButton.Location = new System.Drawing.Point(495, 112);
+            this.exportButton.Location = new System.Drawing.Point(566, 149);
+            this.exportButton.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.exportButton.Name = "exportButton";
-            this.exportButton.Size = new System.Drawing.Size(75, 23);
+            this.exportButton.Size = new System.Drawing.Size(86, 31);
             this.exportButton.TabIndex = 2;
             this.exportButton.Text = "Export list";
             this.exportButton.UseVisualStyleBackColor = true;
@@ -64,9 +66,10 @@ namespace ChillExe.Controls
             // 
             // importButton
             // 
-            this.importButton.Location = new System.Drawing.Point(495, 155);
+            this.importButton.Location = new System.Drawing.Point(566, 207);
+            this.importButton.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.importButton.Name = "importButton";
-            this.importButton.Size = new System.Drawing.Size(75, 23);
+            this.importButton.Size = new System.Drawing.Size(86, 31);
             this.importButton.TabIndex = 3;
             this.importButton.Text = "Import list";
             this.importButton.UseVisualStyleBackColor = true;
@@ -74,9 +77,10 @@ namespace ChillExe.Controls
             // 
             // cleanButton
             // 
-            this.cleanButton.Location = new System.Drawing.Point(495, 198);
+            this.cleanButton.Location = new System.Drawing.Point(566, 264);
+            this.cleanButton.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.cleanButton.Name = "cleanButton";
-            this.cleanButton.Size = new System.Drawing.Size(75, 23);
+            this.cleanButton.Size = new System.Drawing.Size(86, 31);
             this.cleanButton.TabIndex = 4;
             this.cleanButton.Text = "Clean list";
             this.cleanButton.UseVisualStyleBackColor = true;
@@ -84,9 +88,10 @@ namespace ChillExe.Controls
             // 
             // downloadButton
             // 
-            this.downloadButton.Location = new System.Drawing.Point(198, 293);
+            this.downloadButton.Location = new System.Drawing.Point(226, 391);
+            this.downloadButton.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.downloadButton.Name = "downloadButton";
-            this.downloadButton.Size = new System.Drawing.Size(170, 23);
+            this.downloadButton.Size = new System.Drawing.Size(194, 31);
             this.downloadButton.TabIndex = 5;
             this.downloadButton.Text = "Download and install exes";
             this.downloadButton.UseVisualStyleBackColor = true;
@@ -98,12 +103,11 @@ namespace ChillExe.Controls
             this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.Url,
             this.LastUpdate});
-            this.dataGridView1.Location = new System.Drawing.Point(15, 23);
-            this.dataGridView1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dataGridView1.Location = new System.Drawing.Point(17, 31);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 29;
-            this.dataGridView1.Size = new System.Drawing.Size(474, 265);
+            this.dataGridView1.Size = new System.Drawing.Size(542, 353);
             this.dataGridView1.TabIndex = 8;
             this.dataGridView1.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellValueChanged);
             // 
@@ -117,13 +121,14 @@ namespace ChillExe.Controls
             // LastUpdate
             // 
             this.LastUpdate.HeaderText = "Last Update";
+            this.LastUpdate.MinimumWidth = 6;
             this.LastUpdate.Name = "LastUpdate";
             this.LastUpdate.ReadOnly = true;
-            this.LastUpdate.Width = 121;
+            this.LastUpdate.Width = 190;
             // 
             // ElementsList
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.downloadButton);
@@ -131,8 +136,9 @@ namespace ChillExe.Controls
             this.Controls.Add(this.importButton);
             this.Controls.Add(this.exportButton);
             this.Controls.Add(this.saveButton);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "ElementsList";
-            this.Size = new System.Drawing.Size(594, 329);
+            this.Size = new System.Drawing.Size(679, 439);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
