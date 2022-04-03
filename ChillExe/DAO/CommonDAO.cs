68c2b86a489d087440825f4d711d9316fa917f3b
Jorge Gutiérrez <34510304+Yorchly@users.noreply.github.com>
2022-04-03 13:24:48 +0200

    CommonDao added

---
+++ ChillExe/DAO/CommonDAO.cs
@@ -0,0 +1,34 @@
+﻿using ChillExe.Models;
+using ChillExe.Services;
+using System.Collections.Generic;
+
+namespace ChillExe.DAO
+{
+    public abstract class CommonDAO : IDAO
+    {
+        private List<App> apps;
+        private IAppService appService;
+
+        public virtual void Init(IAppService appService)
+        {
+            this.appService = appService;
+            apps = this.appService.Get();
+        }
+
+        public virtual List<App> Get() => apps;
+
+        public virtual void Set(App app) => apps.Add(app);
+
+        public virtual void Set(List<App> apps, bool overwrite = false)
+        {
+            if (overwrite)
+                this.apps = apps;
+            else
+                this.apps.AddRange(apps);
+        }
+
+        public virtual void Clear() => apps.Clear();
+
+        public virtual bool Save() => appService.Save(apps);
+    }
+}
---
+++ ChillExe/DAO/IDAO.cs
@@ -0,0 +1,23 @@
+﻿using ChillExe.Models;
+using ChillExe.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChillExe.DAO
+{
+    public interface IDAO
+    {
+        public void Init(IAppService appService);
+
+        public List<App> Get();
+
+        public void Set(App app);
+
+        public void Set(List<App> apps, bool overwrite = false);
+
+        public void Clear();
+
+        public bool Save();
+    }
+}
