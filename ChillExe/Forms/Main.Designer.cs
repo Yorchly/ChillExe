0788a9832eda62df458a73eab35fdfc38d5b197c
Jorge Gutiérrez <34510304+Yorchly@users.noreply.github.com>
2022-04-12 21:29:28 +0200

    Added initial structure and translations

--- ChillExe/Forms/Main.Designer.cs
+++ ChillExe/Forms/Main.Designer.cs
@@ -31,23 +31,167 @@ namespace ChillExe
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.appsGridView = new System.Windows.Forms.DataGridView();
+            this.UrlColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.LastUpdatedColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.languageDropDown = new System.Windows.Forms.ToolStripDropDownButton();
+            this.spanishDropdownMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.englishDropdownMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.downloadAndInstallButton = new System.Windows.Forms.Button();
+            this.importButton = new System.Windows.Forms.Button();
+            this.exportButton = new System.Windows.Forms.Button();
+            this.cleanListButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.appsGridView)).BeginInit();
+            this.toolStrip1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // appsGridView
+            // 
+            this.appsGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.appsGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.UrlColumn,
+            this.LastUpdatedColumn});
+            this.appsGridView.Location = new System.Drawing.Point(12, 37);
+            this.appsGridView.Name = "appsGridView";
+            this.appsGridView.RowHeadersWidth = 51;
+            this.appsGridView.RowTemplate.Height = 29;
+            this.appsGridView.Size = new System.Drawing.Size(905, 629);
+            this.appsGridView.TabIndex = 0;
+            // 
+            // UrlColumn
+            // 
+            this.UrlColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.UrlColumn.HeaderText = "Url";
+            this.UrlColumn.MinimumWidth = 6;
+            this.UrlColumn.Name = "UrlColumn";
+            // 
+            // LastUpdatedColumn
+            // 
+            this.LastUpdatedColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.LastUpdatedColumn.HeaderText = "Last Updated";
+            this.LastUpdatedColumn.MinimumWidth = 6;
+            this.LastUpdatedColumn.Name = "LastUpdatedColumn";
+            this.LastUpdatedColumn.ReadOnly = true;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.languageDropDown});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.Professional;
+            this.toolStrip1.Size = new System.Drawing.Size(1197, 27);
+            this.toolStrip1.TabIndex = 1;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // languageDropDown
+            // 
+            this.languageDropDown.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.languageDropDown.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.spanishDropdownMenuItem,
+            this.englishDropdownMenuItem});
+            this.languageDropDown.Image = ((System.Drawing.Image)(resources.GetObject("languageDropDown.Image")));
+            this.languageDropDown.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.languageDropDown.Name = "languageDropDown";
+            this.languageDropDown.Size = new System.Drawing.Size(88, 24);
+            this.languageDropDown.Text = "Language";
+            // 
+            // spanishDropdownMenuItem
+            // 
+            this.spanishDropdownMenuItem.Name = "spanishDropdownMenuItem";
+            this.spanishDropdownMenuItem.Size = new System.Drawing.Size(143, 26);
+            this.spanishDropdownMenuItem.Text = "Spanish";
+            this.spanishDropdownMenuItem.Click += new System.EventHandler(this.spanishDropdownMenuItem_Click);
+            // 
+            // englishDropdownMenuItem
+            // 
+            this.englishDropdownMenuItem.Name = "englishDropdownMenuItem";
+            this.englishDropdownMenuItem.Size = new System.Drawing.Size(143, 26);
+            this.englishDropdownMenuItem.Text = "English";
+            this.englishDropdownMenuItem.Click += new System.EventHandler(this.englishDropdownMenuItem_Click);
+            // 
+            // downloadAndInstallButton
+            // 
+            this.downloadAndInstallButton.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.downloadAndInstallButton.Location = new System.Drawing.Point(959, 199);
+            this.downloadAndInstallButton.Name = "downloadAndInstallButton";
+            this.downloadAndInstallButton.Size = new System.Drawing.Size(209, 36);
+            this.downloadAndInstallButton.TabIndex = 2;
+            this.downloadAndInstallButton.Text = "Download and install";
+            this.downloadAndInstallButton.UseVisualStyleBackColor = true;
+            this.downloadAndInstallButton.Click += new System.EventHandler(this.downloadAndInstallButton_Click);
+            // 
+            // importButton
+            // 
+            this.importButton.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.importButton.Location = new System.Drawing.Point(959, 262);
+            this.importButton.Name = "importButton";
+            this.importButton.Size = new System.Drawing.Size(209, 36);
+            this.importButton.TabIndex = 3;
+            this.importButton.Text = "Import list";
+            this.importButton.UseVisualStyleBackColor = true;
+            this.importButton.Click += new System.EventHandler(this.importButton_Click);
+            // 
+            // exportButton
+            // 
+            this.exportButton.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.exportButton.Location = new System.Drawing.Point(959, 327);
+            this.exportButton.Name = "exportButton";
+            this.exportButton.Size = new System.Drawing.Size(209, 36);
+            this.exportButton.TabIndex = 4;
+            this.exportButton.Text = "Export list";
+            this.exportButton.UseVisualStyleBackColor = true;
+            this.exportButton.Click += new System.EventHandler(this.exportButton_Click);
+            // 
+            // cleanListButton
+            // 
+            this.cleanListButton.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.cleanListButton.Location = new System.Drawing.Point(959, 396);
+            this.cleanListButton.Name = "cleanListButton";
+            this.cleanListButton.Size = new System.Drawing.Size(209, 36);
+            this.cleanListButton.TabIndex = 5;
+            this.cleanListButton.Text = "Clean list";
+            this.cleanListButton.UseVisualStyleBackColor = true;
+            this.cleanListButton.Click += new System.EventHandler(this.cleanListButton_Click);
             // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(911, 603);
+            this.ClientSize = new System.Drawing.Size(1197, 688);
+            this.Controls.Add(this.cleanListButton);
+            this.Controls.Add(this.exportButton);
+            this.Controls.Add(this.importButton);
+            this.Controls.Add(this.downloadAndInstallButton);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.appsGridView);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "Main";
             this.Text = "ChillExe";
+            ((System.ComponentModel.ISupportInitialize)(this.appsGridView)).EndInit();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-        private ApplicationsInstallerControl programsControl;
+        private System.Windows.Forms.DataGridView appsGridView;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripDropDownButton languageDropDown;
+        private System.Windows.Forms.ToolStripMenuItem spanishDropdownMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem englishDropdownMenuItem;
+        private System.Windows.Forms.Button downloadAndInstallButton;
+        private System.Windows.Forms.Button importButton;
+        private System.Windows.Forms.Button exportButton;
+        private System.Windows.Forms.Button cleanListButton;
+        private System.Windows.Forms.DataGridViewTextBoxColumn UrlColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn LastUpdatedColumn;
     }
 }
 
--- ChillExe/Forms/Main.cs
+++ ChillExe/Forms/Main.cs
@@ -1,7 +1,9 @@
 ﻿using ChillExe.DAO;
+using ChillExe.Localization;
 using ChillExe.Logger;
 using ChillExe.Models;
 using ChillExe.Services;
+using System;
 using System.Collections.Generic;
 using System.Windows.Forms;
 
@@ -9,9 +11,63 @@ namespace ChillExe
 {
     public partial class Main : Form
     {
-        public Main(ICustomLogger logger, IService<Apps> xmlService)
+        private IStringLocalizer stringLocalizer;
+        private ICustomLogger customLogger;
+        private IService<Apps> xmlService;
+
+        public Main(ICustomLogger logger, IService<Apps> xmlService, IStringLocalizer stringLocalizer)
         {
+            customLogger = logger;
+            this.xmlService = xmlService;
+            this.stringLocalizer = stringLocalizer;
+
             InitializeComponent();
+            GetTranslations();
+        }
+
+        private void GetTranslations()
+        {
+            languageDropDown.Text = stringLocalizer.GetTranslation("LanguageDropdown", "Language");
+            englishDropdownMenuItem.Text = stringLocalizer.GetTranslation("EnglishDropdownMenuItem", "English");
+            spanishDropdownMenuItem.Text = stringLocalizer.GetTranslation("SpanishDropdownMenuItem", "Spanish");
+            importButton.Text = stringLocalizer.GetTranslation("ImportButton", "Import list");
+            exportButton.Text = stringLocalizer.GetTranslation("ExportButton", "Export list");
+            downloadAndInstallButton.Text = stringLocalizer.GetTranslation("DownloadAndInstallButton", "Download and install");
+            cleanListButton.Text = stringLocalizer.GetTranslation("CleanButton", "Clean list");
+            UrlColumn.HeaderText = stringLocalizer.GetTranslation("Url", "Url");
+            UrlColumn.ToolTipText = stringLocalizer.GetTranslation("UrlTooltipText", "Url column");
+            LastUpdatedColumn.HeaderText = stringLocalizer.GetTranslation("LastUpdated", "Last updated");
+            LastUpdatedColumn.ToolTipText = stringLocalizer.GetTranslation("LastUpdatedTooltipText", "Last updated column");
+        }
+
+        private void englishDropdownMenuItem_Click(object sender, System.EventArgs e)
+        {
+
+        }
+
+        private void spanishDropdownMenuItem_Click(object sender, System.EventArgs e)
+        {
+
+        }
+
+        private void downloadAndInstallButton_Click(object sender, System.EventArgs e)
+        {
+
+        }
+
+        private void importButton_Click(object sender, System.EventArgs e)
+        {
+
+        }
+
+        private void exportButton_Click(object sender, System.EventArgs e)
+        {
+
+        }
+
+        private void cleanListButton_Click(object sender, System.EventArgs e)
+        {
+
         }
     }
 }
