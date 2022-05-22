e61121e82dbaae9afaafe1173a9969c5a8f87a5a
Jorge Gutiérrez <34510304+Yorchly@users.noreply.github.com>
2022-05-22 21:21:59 +0200

    AppInstaller added

---
+++ ChillExe/Wrappers/ProcessWrapper.cs
@@ -0,0 +1,38 @@
+﻿using ChillExe.Logger;
+using ChillExe.Models;
+using System;
+using System.Diagnostics;
+
+namespace ChillExe.Wrappers
+{
+    public class ProcessWrapper : IProcessWrapper
+    {
+        private readonly ICustomLogger logger;
+
+        public ProcessWrapper(ICustomLogger logger) => this.logger = logger;
+        
+        public bool Install(string downloadedAppPath)
+        {
+            try
+            {
+                var process = new Process();
+                process.StartInfo.FileName = downloadedAppPath;
+                process.StartInfo.UseShellExecute = false;
+                process.StartInfo.Verb = "runas";
+                process.Start();
+                process.WaitForExit();
+
+                return true;
+            }
+            catch(Exception ex)
+            {
+                logger.WriteLine(
+                    $"Error in ProcessWrapper.Install -> {ex.Message}", 
+                    LogLevel.ERROR
+                );
+
+                return false;
+            }
+        }
+    }
+}
